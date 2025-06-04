5ee7b932a003de8182cc7869b4e622ac969ebfe9
rchicasc201 <rialbert2006@gmail.com>
2025-06-04 11:43:58 -0600

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+
+import 'package:flutter/material.dart';
+import 'package:vibration/vibration.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Vibrapp',
+      home: Scaffold(
+        appBar: AppBar(title: Text('Vibrapp')),
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () async {
+              if (await Vibration.hasVibrator() ?? false) {
+                Vibration.vibrate(pattern: [0, 300, 200, 300]);
+              }
+            },
+            child: Text('Vibrar'),
+          ),
+        ),
+      ),
+    );
+  }
+}
